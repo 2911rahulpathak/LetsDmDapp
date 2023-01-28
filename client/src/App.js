cd7a2b2b3e9be00a3b79184a6f1a8892e57fce30
Rahul <prathampathak14@gmail.com>
2023-01-28 15:06:16 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,72 @@
+import abi from "./contract/chai.json";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import Buy from "./components/Buy";
+import Memos from "./components/Memos";
+
+import "./App.css";
+
+function App() {
+  const [state, setState] = useState({
+    provider: null,
+    signer: null,
+    contract: null,
+  });
+  const [account, setAccount] = useState("None");
+  useEffect(() => {
+    const connectWallet = async () => {
+      const contractAddress = "0x57CF8992371942B8D3ecfaE140c731e945b71ED7";
+      const contractABI = abi.abi;
+      try {
+        const { ethereum } = window;
+
+        if (ethereum) {
+          const account = await ethereum.request({
+            method: "eth_requestAccounts",
+          });
+
+          window.ethereum.on("chainChanged", () => {
+            window.location.reload();
+          });
+
+          window.ethereum.on("accountsChanged", () => {
+            window.location.reload();
+          });
+
+          const provider = new ethers.providers.Web3Provider(ethereum);
+          const signer = provider.getSigner();
+          const contract = new ethers.Contract(
+            contractAddress,
+            contractABI,
+            signer
+          );
+          setAccount(account);
+          setState({ provider, signer, contract });
+        } else {
+          alert("Please install metamask");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    connectWallet();
+  }, []);
+  // console.log(state);
+  return (
+    <div style={{ backgroundColor: "#EFEFEF", height: "100%" }}>
+      
+      <p
+        class="text-muted lead "
+        style={{ marginTop: "10px", marginLeft: "5px" }}
+      >
+        <small>Connected Account - {account}</small>
+      </p>
+      <div className="container">
+        <Buy state={state} />
+        <Memos state={state} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
